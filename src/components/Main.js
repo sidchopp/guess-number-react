ebb935415289b3540e0701a59569e94ca0099650
Sid <sidchopp@gmail.com>
2021-09-09 12:19:08 -0600

    main created

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function HI() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+function Main() {
+  const classes = useStyles();
   return (
-    <div>
-      HI
+    <div style={{ padding: '20px' }}>
+      <div ><TextField
+        id="filled-number"
+        label="Number"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="filled"
+      />
+      </div>
+      <div style={{ padding: '10px' }}>
+        <Button variant="contained" color="primary">
+          Check!
+        </Button>
+      </div>
+      <div style={{ padding: '20px' }}>
+        <div>
+          Start Guessing..
+        </div>
+        <div>
+          Score
+        </div>
+        <div>
+          Highest Score
+        </div>
+      </div>
+
+
     </div>
   )
 }
 
-export default HI
+export default Main
