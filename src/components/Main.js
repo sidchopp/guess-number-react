bd94c3a2022924119e8de9f38b1f67eabed2a91e
Sid <sidchopp@gmail.com>
2021-09-10 15:29:32 -0600

    check button disabled if no number is filled

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,26 +17,27 @@ function Main() {
 
   console.log("Secret Number:", secretNumber);
 
-
+  // Event Handlers
 
   function checkGuess(e) {
     e.preventDefault();
 
+    let Guess = Number(myGuess);
+    console.log("My Guess:", Guess)
 
-    let Guess = Number(myGuess)
     if (Guess === secretNumber) {
       setResult("Congratulations! You got it right!");
       setLowOrHi("");
-
+      setMyGuess(0);
     } else if (Guess > secretNumber) {
       setResult("Wrong!");
       setLowOrHi("Last guess was too high!");
     } else if (Guess < secretNumber) {
       setResult("Wrong!");
       setLowOrHi("Last guess was too low!");
+    } else if (Guess == "" || Guess == 0) {
+      alert("Please fill a number greater than 0");
     }
-
-
   }
 
   function changeHandler(e) {
@@ -44,26 +45,31 @@ function Main() {
     setMyGuess(e.target.value)
   }
 
+  function resetGame() {
+
+  }
+
 
   return (
     <div style={{ padding: '20px' }}>
-
       <form>
-        <label htmlFor={myGuess}>Guess a Number:</label>
-        <input id={myGuess} name={myGuess} type="number" min="1" max="100" onChange={changeHandler} />
+        <label htmlFor={myGuess}>Write a Number to continue:</label>
+        <input placeholder="1 - 100" id={myGuess} name={myGuess} value={myGuess} type="number" min="1" max="100" onChange={changeHandler} required />
         <div style={{ padding: '10px' }}>
-          <Button type='submit' onClick={checkGuess} variant="contained" color="primary" >
+          <Button disabled={!myGuess} type='submit' onClick={checkGuess} variant="contained" color="primary" >
             Check!
           </Button>
+
         </div>
       </form>
-
       <div>
         {console.log(myGuess)}
         {/* <p >{myGuess}</p> */}
         <p >{result}</p>
         <p >{lowOrHi}</p>
-
+        <Button type='submit' onClick={resetGame} variant="contained" color="secondary" >
+          Restart
+        </Button>
       </div>
     </div>
   )
